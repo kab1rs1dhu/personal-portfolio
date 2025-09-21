c448cc769bd89ce1eb43bc14a91d172cd1ee6700
Kabir Sidhu <kxbir414@gmail.com>
2025-09-20 20:55:52 -0700

    Update portfolio: Add components, modify styles and remove unused assets

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import "./Navbar.css"
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <img src="" alt="" />
+            <ul className="nav-menu">
+                <li>Home</li>
+                <li>About</li>
+                <li>Projects</li>
+                <li>Contact</li>
+                <li>Resume</li>
+            </ul>
+            <div className="nav-connect"> Connect with me </div>
+        </div>
+    )
+}
+
+export default Navbar
